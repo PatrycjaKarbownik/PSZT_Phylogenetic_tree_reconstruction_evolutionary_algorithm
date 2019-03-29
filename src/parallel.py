d60399e4d26a73ac6bb56c92c748531c15cd4ffe
PatrycjaKarbownik <patrycja@karbownik.net>
2019-03-29 22:43:30 +0100

    beginning of implementation

---
+++ src/parallel.py
@@ -0,0 +1,13 @@
+starting_gap_penalty = -5
+extending_gap_penalty = -5
+A = 0
+G = 1
+C = 2
+T = 3
+
+subtitutionMatrix = [[10, -1, -3, -4],
+                     [-1,  7, -5, -3],
+                     [-3, -5,  9,  0],
+                     [-4, -3,  0,  8]]
+
+
