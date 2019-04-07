9c0d4b3d385e4f5c910d62c368828e0a1532733c
Nestus97 <K.B.Grabowski@stud.elka.pw.edu.pl>
2019-04-07 13:31:18 +0200

    subtrees comparing

--- src/nodes.py
+++ src/nodes.py
@@ -1,11 +1,36 @@
+names_of_sequences = []
+
+def loadname():
+    n = 0
+    with open("../data/sequences.txt", "r") as file:
+        for line in file:
+            n += 1
+            if n % 2 == 1:
+                first_line = line.rstrip(' ').split()
+                name = first_line[0]
+            else:
+                names_of_sequences.append(name)
+    file.close()
+
+loadname()
+
 class Leaf:
     def __init__(self, name, year):
         self.name = name
         self.year = year
+        self.number = 0
+        for i, sequence_name in enumerate(names_of_sequences):
+            if(sequence_name==name):
+                self.number = 2**i
+        if (self.number==0):
+            print("PRZYPAL...")
 
     def __str__(self):
         string = self.name + ' ' + str(self.year)
         return string
+
+    def __eq__(self, other):
+        return self.number == other.number
 
 class TmpLeaf:
     def __init__(self, name, year, sequence):
@@ -17,12 +42,17 @@ class TmpLeaf:
         string = self.name + ' ' + str(self.year) + ' ' + self.sequence
         return string
 
-
 class Node:
     def __init__(self, left, right):
         self.left = left
         self.right = right
+        self.number = left.number + right.number
 
     def __str__(self):
         string = "left: " + str(self.left) + " right: " + str(self.right)
         return string
+
+    def __eq__(self, other):
+        if(self.number != other.number):
+            return False
+        return ((self.left == other.left)and(self.right == other.right))or((self.right == other.left)and(self.left == other.right))
